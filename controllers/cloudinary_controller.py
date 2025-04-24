86cd86659ccff2cb736ceaf5704baca5c1961e90
Sara Bremer <sbremer@ue-ac.com>
2025-04-24 05:49:37 -0600

    Trying to update with 2nd API pics again after failed update to GIT

--- controllers/cloudinary_controller.py
+++ controllers/cloudinary_controller.py
@@ -12,10 +12,6 @@ CLOUD_NAME = os.getenv("CLOUDINARY_CLOUD_NAME")
 API_KEY = os.getenv("CLOUDINARY_API_KEY")
 API_SECRET = os.getenv("CLOUDINARY_API_SECRET")
 
-print("CLOUDINARY_CLOUD_NAME =", CLOUD_NAME)
-print("CLOUDINARY_API_KEY =", API_KEY) 
-print("CLOUDINARY_API_SECRET =", API_SECRET)
-
 @router.get("/cloudinary/images/{folder}")
 def get_images_by_folder(folder: str):
     print(f"Fetching images for folder: {folder}")
@@ -32,6 +28,5 @@ def get_images_by_folder(folder: str):
     if response.status_code != 200:
         print(f"FAILED: {response.status_code} - {response.text}")
         return {"error": "Failed to fetch from Cloudinary"}
-    
-    
+        
     return response.json()
