7367515d2fc85f57288b7b5934243cf1f9749717
Sara Bremer <sbremer@ue-ac.com>
2025-04-25 06:03:10 -0600

    Updating CORS

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware, 
-    allow_origins = ["http://localhost:5173"],
+    allow_origins = ["http://localhost:5173", "http://localhost:3000", "https://ddzxflqkt9iqp.cloudfront.net"],
     allow_credentials=True,
     allow_methods=["*"] ,
     allow_headers=["*"]
