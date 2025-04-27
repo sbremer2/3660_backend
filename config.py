994e55c776bf051db77f2f95abf2b3959367f0e5
Sara Bremer <sbremer@ue-ac.com>
2025-04-27 01:46:43 -0600

    Updating config.py to include all .env variables

--- config.py
+++ config.py
@@ -4,10 +4,13 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     app_env: str
     allow_origins: list[AnyHttpUrl]
+    cloudinary_cloud_name: str
+    cloudinary_api_key: str
+    cloudinary_api_secret: str
+    pexels_api_key: str
     api_token: str
     
     class Config: 
         env_file = ".env"
-        
         
 settings = Settings()
